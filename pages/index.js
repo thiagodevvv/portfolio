c879467dfbfafacfd31c4f6172b34a14978aafb4
lemao-code <th.ferrari@outlook.com.br>
2021-08-10 17:14:11 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,83 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import {Container} from 'react-bootstrap'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <body>
+        <Container className="container" fluid>
+          <Head>
+            <title>Thiago - Programador</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Container className="header" fluid>
+            <Container className="nav-bar">
+              <Container style={{flexDirection: "row", flex: 1}}>
+                <p style={{fontFamily: 'Oswald', fontSize: 15, color: 'white'}}>Thiago</p> 
+                <p style={{fontFamily: 'Oswald', fontSize: 15, color: 'white', opacity: 0.7, marginLeft: 3}}>Ferrari</p>
+              </Container>
+              <Container style={{flexDirection: "row", flex:3}}>
+                <p style={{fontFamily: 'Oswald', fontSize: 15, color: 'white'}}>Apresentação</p>
+                <p style={{fontFamily: 'Oswald', fontSize: 15, color: 'white', marginLeft: 20}}>Sobre mim</p>
+                <p style={{fontFamily: 'Oswald', fontSize: 15, color: 'white', marginLeft: 20}}>Por que devo ser contratado</p>
+              </Container>
+              <Container style={{flexDirection: "row", flex: 2}}>
+                <Container style={{backgroundColor: "white", height: 30, width: 30, borderRadius: 5, display: 'flex', alignItems: 'center', marginRight: 5}}>
+                  <img src="/bxl-github.svg" width="30" height="30"></img>
+                </Container>
+                <p style={{fontSize: 'Oswald', fontSize: 13, color: 'white', marginTop: 5}}>GitHub</p>
+                <Container style={{backgroundColor: "white", height: 30, width: 30, borderRadius: 5,display: 'flex', alignItems: 'center',  marginRight: 5}}>
+                  <img src="/bxl-linkedin-square.svg" width="30" height="30"></img>
+                </Container>
+                <p style={{fontSize: 'Oswald', fontSize: 13, color: 'white',marginTop: 5}}>Linkedin</p>
+                <Container style={{backgroundColor: "white", height: 30, width: 30, borderRadius: 5,display: 'flex', alignItems: 'center',  marginRight: 5}}>
+                  <img src="/bx-mail-send.svg" width="30" height="30"></img>
+                </Container>
+                <p style={{fontSize: 'Oswald', fontSize: 13, color: 'white', marginTop: 5}}>Send mail</p>
+              </Container>
+            </Container>{/* */}
+            <Container className="about">
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+              <Container className="about-text">
+                <Container style={{backgroundColor: "rgb(111,244,168)", margin: 0, padding:0, width: 160, fontWeight: 'bold'}}>
+                  <p style={{fontFamily: 'Roboto', fontSize: 15, margin: 4, padding: 0}}>Programador FullStack</p>
+                </Container>
+                <p align="justify" style={{marginTop: 30,fontSize: 15, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white'}}>Meu nome é Thiago, tenho 24 anos, 
+                moro em Bauru-SP e estou em busca de emprego na área de desenvolvimento.</p>
+              </Container>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+              <Container className="content-img">
+                <Image src="/hack.png" width="300" height="380" responsive="true"/>
+              </Container>
+            </Container> 
+              
+          </Container>
+          <Container className="content" fluid>
+            {/* Container dos butoes */}
+            <Container style={{display: 'flex', flexDirection : 'column', width: "50%", marginLeft:0}}>
+              <Container className="backend">
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 25, color: 'white'}}>Back-end</p>
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 13, color: 'white', opacity: 0.8}}>Clique para conhecer</p>
+              </Container>
+              <Container className="frontend">
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 25, color: 'white'}}>Front-end</p>
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 13, color: 'white', opacity: 0.8}}>Clique para conhecer</p>
+              </Container>
+              <Container className="db">
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 25, color: 'white'}}>Banco de dados</p>
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 13, color: 'white', opacity: 0.8}}>Clique para conhecer</p>
+              </Container>
+              <Container className="projects">
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 25, color: 'white'}}>Projetos</p>
+                <p style={{fontFamily: 'Roboto', fontWeight: 'bold', fontSize: 13, color: 'white', opacity: 0.8}}>Clique para conhecer</p>
+              </Container>
+            </Container>
+           {/* Container dos textos */}
+           <Container style={{width: "50%"}}>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+           </Container>
+          </Container>
+    </Container>
+      </body>
   )
 }
