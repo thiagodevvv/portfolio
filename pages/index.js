6dcd5bd3edc867c94a60c548e9c9cc929fd12eae
lemao-code <th.ferrari@outlook.com.br>
2021-08-10 22:54:28 -0300

    aprensetação, sobre mim e motivos para me contratar finalizados

--- pages/index.js
+++ pages/index.js
@@ -42,19 +42,19 @@ export default function Home() {
                 }} style={{color: `${motivosVisible ? "rgb(111,244,168)" : ""}`}} className="menu-text">Motivos para me contratar</p>
               </Container>
               <Container style={{flexDirection: "row", flex: 2}}>
-                <Container className="content-social-img-text">
+                <Container onClick={() => window.open('https://github.com/thiagodevvv','_blank').focus()} className="content-social-img-text">
                   <Container  className="content-social">
                   <img src="/bxl-github.svg" width="30" height="30"></img>
                   </Container>
                   <p className="social-text">GitHub</p>
                 </Container>
-                <Container className="content-social-img-text">
+                <Container onClick={() => window.open('https://www.linkedin.com/in/thiago-ferrari-b90996193/','_blank').focus()} className="content-social-img-text">
                   <Container className="content-social">
                     <img src="/bxl-linkedin-square.svg" width="30" height="30"></img>
                   </Container>
                   <p className="social-text">LinkedIn</p>
                 </Container>
-                <Container className="content-social-img-text">
+                <Container  onClick={() => window.open('https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=th.ferrari@outlook.com.br', '_blank').focus()} className="content-social-img-text">
                   <Container className="content-social">
                   <img src="/bx-mail-send.svg" width="30" height="30"></img>
                   </Container>
@@ -69,12 +69,28 @@ export default function Home() {
 
               <Container className="about-text">
                 <Container style={{backgroundColor: "rgb(111,244,168)", margin: 0, padding:0, width: 160, fontWeight: 'bold', borderRadius: 4}}>
-                  <p style={{fontFamily: 'Roboto', fontSize: 15, margin: 4, padding: 0}}>Programador FullStack</p>
+                 {apresentacaoVisible ? <p style={{fontFamily: 'Roboto', fontSize: 15, margin: 4, padding: 0}}>Programador FullStack</p> : ""}
                 </Container>
-                {apresentacaoVisible ? <p align="justify" style={{marginTop: 30,fontSize: 15, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white'}}>Meu nome é Thiago, tenho 24 anos, 
-                moro em Bauru-SP e estou em busca de emprego na área de desenvolvimento.</p> : ""}
-                {sobreMimVisible ? <p align="justify" style={{marginTop: 30,fontSize: 15, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> : ""}
-                {motivosVisible ? <p align="justify" style={{marginTop: 30,fontSize: 15, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white'}}>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p> : ""}
+                {apresentacaoVisible ? <p align="justify" style={{marginTop: 30,fontSize: 15, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white',letterSpacing: 0.8}}>
+                Oi, seja bem vindo ao meu portfolio dev!<br/><br/> Me chamo Thiago, tenho 24 anos, moro em Bauru-SP e estou em busca de emprego na área de desenvolvimento.</p> : ""}
+                {sobreMimVisible ? <p align="justify" style={{marginTop: 10,fontSize: 14, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white', letterSpacing: 0.8}}>
+                Comecei a estudar programação em setembro de 2019. Mas por que eu comecei a estudar programação? 
+                Em 2019 eu tinha 22 anos e trabalhando no atual emprego (Em um almoxarifado hospitalar) decidi que queria trabalhar com algo que eu me identificasse, 
+                que gostasse... Sempre gostei de computador, de jogos, criar servidores piratas (Jogo The Duel) onde tive o primeiro contato de como as coisas funcionavam, 
+                editando sites já prontos feitos em php+javascript+html+css, arquivos de xml, SQL Server entre outros. 
+                Então coloquei como meu objetivo de carreira profissional que me tornaria um desenvolvedor.
+                Além dos estudos pela internet(youtube,udemy entre outros) iniciei graduação  na faculdade UNIP cursando Análise de Desenvolvimento de Sistemas em janeiro de 2020. 
+                Sempre fui empenhado nos estudos, autodidata e disciplinado, mesmo estando 10 horas fora de casa trabalhando em meu atual emprego me mantive em busca de 
+                conhecimento e colocando em pratica.
+                  </p> : ""}
+                {motivosVisible ? <p align="justify" style={{marginTop: 10,fontSize: 15, fontFamily: 'Roboto', fontWeight: 'bold', color: 'white',letterSpacing: 0.8}}>
+                  <ul>
+                    <li> <p style={{fontFamily: 'Roboto', fontSize: 15, fontWeight: 'bold', color: 'white'}}>Sede de aprendizado e crescimento profissional</p></li>
+                    <li> <p style={{fontFamily: 'Roboto', fontSize: 15, fontWeight: 'bold', color: 'white'}}>Programador raiz, escolho a ferramenta de linguagem de programação ideal para resolver o problema</p></li>
+                    <li> <p style={{fontFamily: 'Roboto', fontSize: 15, fontWeight: 'bold', color: 'white'}}>Aprendo lendo documentação da ferramenta/tecnologia</p></li>
+                    <li> <p style={{fontFamily: 'Roboto', fontSize: 15, fontWeight: 'bold', color: 'white'}}>Forte hábito de pesquisa em inglês para me auxiliar em dificuldades</p></li>
+                  </ul>
+                  </p> : ""}
               </Container>
 
               <Container className="content-img">
